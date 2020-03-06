437d59df86d077f88e237a4fc4d560be9bb9f94f
Qamar Abbas <32230195+qamarabbas408@users.noreply.github.com>
2020-03-06 22:07:21 +0500

    Add files via upload

---
+++ Books-online/main.js
@@ -0,0 +1,14 @@
+let navbar=$(".navbar");
+$(window).scroll(function(){
+    // console.log(window.innerHeight);
+    // console.log($(".section-2").offset().top);
+    let oTop=$(".section-2").offset().top-window.innerHeight;
+    // console.log(oTop);
+    if($(window).scrollTop()>oTop){
+        navbar.addClass("sticky");
+    }
+    else{
+        navbar.removeClass("sticky");
+    }
+});
+
